cd6f77042855ec066305bccd52dd5725ab5e8540
MH Rabbi <42906079+MoshiulRabbi@users.noreply.github.com>
2020-09-26 20:36:23 +0600

    Database Timeout increase

--- project4/settings.py
+++ project4/settings.py
@@ -74,11 +74,15 @@ WSGI_APPLICATION = 'project4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'OPTIONS': {
+    # ...
+    'timeout': 20,
+    # ...
 }
 
 AUTH_USER_MODEL = "network.User"
